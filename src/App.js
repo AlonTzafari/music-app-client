be3a71ff7fdef462f3a7fda1b2fc7cb76d5d2cd6
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-04 19:10:41 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Song from './components/Song';
+import Album from './components/Album';
+import Artist from './components/Artist';
+import Playlist from './components/Playlist';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/song/:id" component={Song}/>
+          <Route path="/album/:id" component={Album}/>
+          <Route path="/artist/:id" component={Artist}/>
+          <Route path="/playlist/:id" component={Playlist}/>
+          <Route component={() => <h1>404 Not Found</h1>}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
