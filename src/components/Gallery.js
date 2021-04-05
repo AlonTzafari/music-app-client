f81fe8020a0e459399b883fb1b87bf1e84d57c30
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-05 10:28:16 +0300

    gallery item links

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,3 +14,4 @@ function Gallery({items, type, title}) {
 }
 
 export default Gallery
+
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,17 +1,37 @@
 import albums from '../data/albums.json'
 import '../styles/GalleryItem.css'
+import {Link} from 'react-router-dom'
 
 function GalleryItem({item, type}) {
     const src = type === "songs" ?
         `../images/albums/${albums.find(album => album.name === item.album).cover_img}` :
         `../images/${type}/${item.cover_img}`;
+    //pages
+    const page = getPage(type);
     
     return (
         <li className="galleryItem">
-            <img className="galleryImg" src={src} alt=""/>
-            <p className="description">{item.name}</p>
+            <Link to={`/${page}/${item.id}`}>
+                <img className="galleryImg" src={src} alt=""/>
+                <p className="description">{item.name}</p>
+            </Link>
         </li>
     )
 }
 
 export default GalleryItem
+
+const getPage = itemType => {
+    switch (itemType) {
+        case "songs":
+            return "song";
+        case "albums":
+            return "album";
+        case "artists":
+            return "artist";
+        case "playlists":
+            return "playlist";
+        default:
+            return "unknown";
+    }
+}
