953c36a259c8934b59de6b1afe801247947a054f
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-05 18:27:52 +0300

    lazy loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import Gallery from './Gallery'
+import React, {lazy, Suspense} from 'react'
 import songs from '../data/songs.json'
 import playlists from '../data/playlists.json'
 import artists from '../data/artists.json'
 import albums from '../data/albums.json'
 import '../styles/Home.css'
 import ErrorBoundary from './ErrorBoundary'
+const Gallery = lazy( () => import('./Gallery') );  
 
 function Home() {
     return (
         <div className="home page">
             <ErrorBoundary>
-                <Gallery items={songs} type="songs" title="Top Songs"/>
-                <Gallery items={playlists} type="playlists" title="Top Playlists"/>
-                <Gallery items={artists} type="artists" title="Top Artists"/>
-                <Gallery items={albums} type="albums" title="Top Albums"/>
+                <Suspense fallback={<h1>Loading...</h1>} >
+                    <Gallery items={songs} type="songs" title="Top Songs"/>
+                    <Gallery items={playlists} type="playlists" title="Top Playlists"/>
+                    <Gallery items={artists} type="artists" title="Top Artists"/>
+                    <Gallery items={albums} type="albums" title="Top Albums"/>
+                </Suspense>
             </ErrorBoundary>
         </div>
     )
