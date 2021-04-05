5ee0387f0d6abf3f71589d6fe2cc0d81b19b0c63
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-05 14:34:54 +0300

    album & artist pages

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import albums from '../data/albums.json'
+import '../styles/Album.css'
+import SongList from './SongList'
 
 function Album() {
+    const {id} = useParams();
+    const album = albums.find(album => album.id === id);
     return (
-        <div>
-            
+        <div className="album page">
+            <img src={`../images/albums/${album.cover_img}`} alt=""/>
+            <SongList songList={album.songs} title={album.name}/>
         </div>
     )
 }
--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import artists from '../data/artists.json'
+import '../styles/Artist.css'
+import SongList from './SongList'
 
 function Artist() {
+    const {id} = useParams();
+    const artist = artists.find(artist => artist.id === id);
     return (
-        <div>
-            
+        <div className="artist page">
+            <img src={`../images/artists/${artist.cover_img}`} alt=""/>
+            <SongList songList={artist.songs} title={artist.name}/>
         </div>
     )
 }
 
 export default Artist
+
