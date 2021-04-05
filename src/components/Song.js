c0172ee1547adec8d488d4983bc5429024d53bf7
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-05 11:35:46 +0300

    song player

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
-
+import '../styles/Song.css'
+import songs from '../data/songs.json'
+ 
 function Song(props) {
     const {id} = useParams();
+    const song = songs.find(song => song.id === id);
     return (
         <div className="song page">
-            {`song ID: ${id}`}
+            <div className="contentGrid">
+                <div className="player">
+                    <iframe src={song.youtube_link} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+                <div>{`song list`}</div>
+            </div>
         </div>
     )
 }
