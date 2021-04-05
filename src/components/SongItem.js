e4eb686a956ccd0542ef0dc97e563365b6fe7484
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-05 13:53:08 +0300

    song items link to songs

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../styles/SongItem.css'
+import {Link} from 'react-router-dom'
 
 function SongItem({song}) {
     return (
-        <div className="songItem">
+        <Link className="songItem" to={`/song/${song.id}`}>
             <img src={`http://img.youtube.com/vi/${song.id}/0.jpg`} alt="" />
             <div className="nameArt">
                 <span className="name">{song.name}</span>
                 <span className="artist">{song.artist}</span>
             </div>
             <span className="length">{song.length}</span>
-        </div>
+        </Link>
     )
 }
 
